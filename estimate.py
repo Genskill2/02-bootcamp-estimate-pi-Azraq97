4c358e9f3ce829c0f8a753d4fea1c896f28cb479
Azraq97 <84129096+Azraq97@users.noreply.github.com>
2021-06-02 09:55:36 +0530

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -1,32 +1,39 @@
-import math
-import unittest
-
-class TestWallis(unittest.TestCase):
-    def test_low_iters(self):
-        for i in range(0, 5):
-            pi = wallis(i)
-            self.assertTrue(abs(pi - math.pi) > 0.15, msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
-            
-    def test_high_iters(self):
-        for i in range(500, 600):
-            pi = wallis(i)
-            self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
+def wallis(n):
+    pi = 2
+    for i in range(1, n):
+        left = (2. * i)/(2. * i - 1.)
+        right = (2. * i)/(2. * i + 1.)
+        pi = pi * left * right
+    return pi
+print(wallis(6))
 
 
-class TestMC(unittest.TestCase):
-    def test_randomness(self):
-        pi0 = monte_carlo(15000)
-        pi1 = monte_carlo(15000)
-        
-        self.assertNotEqual(pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
 
-        self.assertFalse(abs(pi0 - pi1) > 0.05, "Two different estimates of PI are too different. This should not happen")
+def monte_carlo(n):
+    import random
 
-    def test_accuracy(self):
-        for i in range(500, 600):
-            pi = monte_carlo(i)
-            self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
-        
-    
-if __name__ == "__main__":
-    unittest.main()
+    INTERVAL = 1000
+
+    circle_points = 0
+    square_points = 0
+
+
+    for i in range(INTERVAL ** 2):
+
+
+        rand_x = random.uniform(-1, 1)
+        rand_y = random.uniform(-1, 1)
+
+        origin_dist = rand_x ** 2 + rand_y ** 2
+
+
+        if origin_dist <= 1:
+            circle_points += 1
+
+        square_points += 1
+
+
+        pi = 4 * circle_points / square_points
+
+
+print("Final Estimation of Pi=", pi)
