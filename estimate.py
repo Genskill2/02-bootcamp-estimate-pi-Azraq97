f57bc027c8fb00673a54415619c249bece4924cb
Azraq97 <84129096+Azraq97@users.noreply.github.com>
2021-06-02 11:28:37 +0530

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -9,31 +9,21 @@ print(wallis(10))
 
 
 
+
+ 
 def monte_carlo(n):
-    import random
-
-    INTERVAL = 1000
-
-    circle_points = 0
-    square_points = 0
-
-
-    for i in range(INTERVAL ** 2):
-        rand_x = random.uniform(-1, 1)
-        rand_y = random.uniform(-1, 1)
-
-        origin_dist = rand_x ** 2 + rand_y ** 2
-
-
-        if origin_dist <= 1:
-            circle_points += 1
-
-        square_points += 1
-
-
-        pi = 4 * circle_points / square_points
-    return pi
-print(monte_carlo(10))
+  import random
+  circle_point=1
+  square_point=0
+  while circle_point<=square_point:
+     x=random.random()
+     y=random.random()
+     distance=mat.sqrt((x**2)+(y**2))
+     if distance<1:
+         square_point +=1
+     circle_point+=1
+  pi=4*(square_point/n)
+  return pi
 
 
 
