8de1fc3c278b3c36a52c217217e4595c04fec248
Azraq97 <84129096+Azraq97@users.noreply.github.com>
2021-06-02 11:32:47 +0530

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -10,9 +10,9 @@ def wallis(n):
 
 
 
- 
+import random 
 def monte_carlo(n):
-  import random
+
   circle_point=1
   square_point=0
   while circle_point<=square_point:
@@ -22,8 +22,8 @@ def monte_carlo(n):
      if distance<1:
          square_point +=1
      circle_point+=1
-  pi=4*(square_point/n)
-  return pi
+   pi=4*(square_point/n)
+   return pi
 
 
 
